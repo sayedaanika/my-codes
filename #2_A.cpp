91371e0e046fdcf57b948a5e4e3abfdd5ccaac78
sayedaanika <anikaani123123@gmail.com>
2025-10-29 09:33:08 +0600

    Create  #2_A.cpp

---
+++ #2_A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+long long n;
+while(true)
+    {
+        cin>>n;
+        if(n == 1999){
+            cout<< "Correct" <<endl;
+        break;}
+        else
+            cout<< "Wrong" <<endl;
+    }
+}
